f3d0ca0531dbd539865b2b21b0cc7974ea2ca658
Ben Agricola <bagricola@squiz.co.uk>
2019-04-29 11:47:56 +0100

    Allow management service to be triggered by start-and-stop files
    Signed-off-by: Ben Agricola <bagricola@squiz.co.uk>

--- management/run.sh
+++ management/run.sh
@@ -1,13 +1,40 @@
 #!/bin/bash
-#
-# Wrapper around beegfs-mgmtd to start the service _and_ keep an eye
-# on the log. beegfs-mgmtd is able to run non-daemonized, but it
-# doesn't report anything on stdout/stderr.
+NODE_ID=${1:-beegfs-mgmtd}
 
-: ${BEEGFS_LOGLEVEL:="3"}
+READY_FILE=/data/.ready
+ACTIVE_FILE=/data/.active
+DATA_PATH=/data/beegfs/test
+WAIT_INTERVAL=1
+
+echo "Waiting for filesystem to become ready..."
+until [ -f "${READY_FILE}" ]; do
+    sleep ${WAIT_INTERVAL}
+done
+
+touch ${ACTIVE_FILE}
+
+if [ ! -f "${DATA_PATH}/format.conf" ]; then
+    echo "Data path ${DATA_PATH} needs initialisation..."
+    /opt/beegfs/sbin/beegfs-setup-mgmtd -C -p ${DATA_PATH} -S ${NODE_ID}
+fi
+
+echo "Starting BeeGFS..."
 
 /opt/beegfs/sbin/beegfs-mgmtd \
-    cfgFile=/etc/beegfs/beegfs-mgmtd.conf \
-    logLevel=${BEEGFS_LOGLEVEL}
+    storeMgmtdDirectory="${DATA_PATH}/" \
+    storeAllowFirstRunInit=false \
+    runDaemonized=False \
+    logLevel=3 &
 
-tail -f /var/log/beegfs-mgmtd.log
+BEEGFS_PID=${!}
+
+while [ -f "${READY_FILE}" ] && (kill -0 ${BEEGFS_PID} 2>/dev/null); do
+    echo "BeeGFS Running..."
+    sleep ${WAIT_INTERVAL}
+done
+
+echo "BeeGFS died or ready marker ${READY_FILE} removed, stopping..."
+kill -15 ${BEEGFS_PID} 2>/dev/null || echo "Unable to send kill to PID ${BEEGFS_PID}, maybe it's already stopped"
+
+echo "BeeGFS stopped, removing active marker"
+rm -f ${ACTIVE_FILE}
